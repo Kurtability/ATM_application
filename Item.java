38ad9ad36b52657d827bfa6ffb3e8c63cd7344d2
Tahsinul Abedin <tabe5206@uni.sydney.edu.au>
2020-11-05 02:48:24 +1100

    Add files via upload
    way to change file descriptions

---
+++ Item.java
@@ -0,0 +1,24 @@
+package LiteSnacks.UI;
+
+public class Item {
+    public int qty;
+    public int price;
+    public String product_name;
+    public String path;
+
+    Item(int qty){
+        this.qty = qty;
+        this.price = 100;
+        this.product_name = "coca";
+        this.path = "coca.jpeg";
+
+    }
+    public void modifyqty( int gap){
+        qty = qty+(gap);
+    }
+    public void modifyprice( int new_price){ price = new_price;}
+    public void modifyname( String nme){
+        product_name=nme;
+    }
+
+}
